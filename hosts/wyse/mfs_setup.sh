8411354cbc58c740e055ab9bc6732e37918a9ecb
Siva Mahadevan <me@svmhdvn.name>
2025-04-26 11:30:29 -0400

    add myfreebsd script to automate dev tasks

--- hosts/wyse/mfs_setup.sh
+++ hosts/wyse/mfs_setup.sh
@@ -3,13 +3,14 @@
 _host_specific_setup() {
   # environment:
   : ${EMPT_ORG_DOMAIN:=empt.test}
+  EMPT_REALM="$(echo "${EMPT_ORG_DOMAIN}" | tr '[:lower:]' '[:upper:]')"
 
   echo '$6$lNfYRw2DzRqbPccf$gH0wn2hv64/QIPtNDRazNok7IdySenCyhYFTUyyZoRXkABvhJnpeUdYSX2PMguUfCvVQ0hTsbDOo5Nd45Ki010' | \
     doas pw -R "${_worlddir}" usermod root -H 0
   doas pw -R "${_worlddir}" useradd mlmmj -c 'mlmmj manager' -d /var/spool/mlmmj -s /usr/sbin/nologin -h -
-  sysrc -f "${_worlddir}/etc/rc.conf.local" hostname="it.${EMPT_ORG_DOMAIN}"
 
-  sed -i'' \
+  grep -R '%%[^%]*%%' -l "${_worlddir}" | xargs sed \
     -e "s,%%EMPT_ORG_DOMAIN%%,${EMPT_ORG_DOMAIN},g" \
-    "${_worlddir}/usr/local/etc/smb4.conf"
+    -e "s,%%EMPT_REALM%%,${EMPT_REALM},g" \
+    -i ''
 }
--- hosts/x470/mfs_setup.sh
+++ hosts/x470/mfs_setup.sh
@@ -5,4 +5,6 @@ _host_specific_setup() {
   echo '$6$602qzC8bCbJebi3L$5xf2vXluEZmZOx7a.aJ5/NzyAyurl6wC1ZTpvYB76QeAabWFPBICyl4TAa8UZ86x8tAfl2prc2992U1enWgCM0' | \
       doas pw -R "${_worlddir}" useradd -n siva -c "Siva Mahadevan" -G wheel -H 0
   grep '^\s*\w' "${ports_pkglist}" | xargs ${_pkg_cmd} install -y -r ports
+  # TODO remove after this fix gets merged upstream
+  doas sed -e 's,${pkg_bin} update -f;,${pkg_bin} update -f -r FreeBSD;,' -i '' "${_worlddir}/usr/local/share/poudriere/common.sh"
 }
