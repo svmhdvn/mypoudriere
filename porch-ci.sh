c507b8fe9794b052dc1f71332e95c8dda716b020
Siva Mahadevan <me@svmhdvn.name>
2025-09-10 14:41:17 -0400

    bak

--- porch-ci.sh
+++ porch-ci.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-set -x
-outdir="$1"
-test_filters="$2"
-vm_name="$(basename ${outdir})"
-disk="${outdir}/disk.raw"
+realpath="$(realpath "$0")"
+top="$(dirname "${realpath}")"
+vm_name="$(basename ${top})"
 
-meta_tar="${outdir}/test-reports.tar"
-meta_dir="${outdir}/test-reports.d"
-meta_dirout="${outdir}/test-reports"
+disk="${top}/disk.raw"
+
+meta_tar="${top}/test-reports.tar"
+meta_dir="${top}/test-reports.d"
+meta_dirout="${top}/test-reports"
 
 mkdir -p "${meta_dir}" "${meta_dirout}"
 truncate -s 512M "${meta_tar}"
 tar rvf "${meta_tar}" -C "${meta_dir}" .
 
 truncate -s 128m \
-  "${outdir}/disk1" \
-  "${outdir}/disk2" \
-  "${outdir}/disk3" \
-  "${outdir}/disk4" \
-  "${outdir}/disk5" \
-  "${outdir}/disk-cam"
+  "${top}/disk1" \
+  "${top}/disk2" \
+  "${top}/disk3" \
+  "${top}/disk4" \
+  "${top}/disk5" \
+  "${top}/disk-cam"
 
 bhyvectl --vm=${vm_name} --destroy || true
 expect -c "spawn /usr/sbin/bhyveload -c stdio -m 4g -d ${disk} ${vm_name};  expect { eof };  exit [lindex [wait] 3]"
@@ -32,12 +32,12 @@ local cmd = "/usr/sbin/bhyve -c 2 -m 4g -A -H -P \z
   -s 1:0,lpc \z
   -s 2:0,virtio-blk,${disk} \z
   -s 3:0,virtio-blk,${meta_tar} \z
-  -s 4:0,ahci-hd,${outdir}/disk-cam \z
-  -s 5:0,virtio-blk,${outdir}/disk1 \z
-  -s 6:0,virtio-blk,${outdir}/disk2 \z
-  -s 7:0,virtio-blk,${outdir}/disk3 \z
-  -s 8:0,virtio-blk,${outdir}/disk4 \z
-  -s 9:0,virtio-blk,${outdir}/disk5 \z
+  -s 4:0,ahci-hd,${top}/disk-cam \z
+  -s 5:0,virtio-blk,${top}/disk1 \z
+  -s 6:0,virtio-blk,${top}/disk2 \z
+  -s 7:0,virtio-blk,${top}/disk3 \z
+  -s 8:0,virtio-blk,${top}/disk4 \z
+  -s 9:0,virtio-blk,${top}/disk5 \z
   -l com1,stdio ${vm_name}"
 for arg in cmd:gmatch("%S+") do
   table.insert(argv, arg)
@@ -45,13 +45,13 @@ end
 spawn(argv)
 
 log("/dev/stdout")
-timeout(60*60*2)
+timeout(60*60)
 
 match "login:"
 write("root\\r")
 
 match("root@.*#")
-write("sysrc freebsdci_test_filters='${test_filters}'\\r")
+write("sysrc freebsdci_test_filters='$(xargs echo < ${top}/test_filters.txt)'\\r")
 
 match("root@.*#")
 write("service freebsdci onestart\\r")
