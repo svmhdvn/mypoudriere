88c7b302520a3124c3d19d5ba3944e4375ab43e0
Siva Mahadevan <me@svmhdvn.name>
2025-09-18 08:57:10 -0400

    bak

---
+++ porch_ci_riscv64.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -x
+
+realpath="$(realpath "$0")"
+top="$(dirname "${realpath}")"
+vm_name="$(basename ${top})"
+
+disk="${top}/disk.raw"
+
+meta_tar="${top}/test-reports.tar"
+meta_dir="${top}/test-reports.d"
+meta_dirout="${top}/test-reports"
+
+mkdir -p "${meta_dir}" "${meta_dirout}"
+truncate -s 512M "${meta_tar}"
+cp "${top}/kyua_filters.txt" "${meta_dir}"
+tar rvf "${meta_tar}" -C "${meta_dir}" .
+
+# TODO figure out QEMU_DEVICES
+#truncate -s 128m \
+#  "${top}/disk1" \
+#  "${top}/disk2" \
+#  "${top}/disk3" \
+#  "${top}/disk4" \
+#  "${top}/disk5" \
+#  "${top}/disk-cam"
+
+timeout -k 1m 1h /usr/local/bin/qemu-system-riscv64 \
+  -machine virt \
+  -smp 2 \
+  -m 4g \
+  -nographic \
+  -no-reboot \
+  -bios /usr/local/share/opensbi/lp64/generic/firmware/fw_jump.elf \
+  -kernel /usr/local/share/u-boot/u-boot-qemu-riscv64/u-boot.bin \
+  -device virtio-blk,drive=hd0 \
+  -device virtio-blk,drive=hd1 \
+  -blockdev driver=raw,node-name=hd0,file.driver=file,file.filename=${disk} \
+  -blockdev driver=raw,node-name=hd1,file.driver=file,file.filename=${meta_tar}
+
+tar xfv ${meta_tar} -C ${meta_dirout}
+rm -rf ${meta_tar} ${meta_dir}
+chmod 0755 ${meta_dirout}
+echo "Extracted kyua reports to ${meta_dirout}"
